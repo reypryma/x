89cdd5f5e6ea8668e72d77cea51c6395592f9807
reypryma <refanopradana@gmail.com>
2023-11-01 07:31:50 +0700

    add drawer

---
+++ lib/features/home/widgets/side_drawer.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:x/common/common.dart';
+import 'package:x/constants/constants.dart';
+import 'package:x/features/auth/controller/auth_controller.dart';
+import 'package:x/features/profile/controller/profile_controller.dart';
+import 'package:x/features/profile/view/user_profile_view.dart';
+import 'package:x/theme/pallete.dart';
+
+class SideDrawer extends ConsumerWidget {
+  const SideDrawer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentUser = ref.watch(currentUserDetailsProvider).value;
+
+    if (currentUser == null) {
+      return const LoadingWidget();
+    }
+
+    return SafeArea(
+      child: Drawer(
+        backgroundColor: Pallete.backgroundColor,
+        child: Column(
+          children: [
+            const SizedBox(height: 50),
+            ListTile(
+              leading: const Icon(
+                Icons.person,
+                size: 30,
+              ),
+              title: const Text(
+                'My Profile',
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  UserProfileView.route(currentUser),
+                );
+              },
+            ),
+            ListTile(
+              leading: SvgPicture.asset(
+                AssetsConstants.twitterLogo,
+                height: 30,
+                color: Colors.white,
+              ),
+              title: const Text(
+                'Twitter Blue',
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              onTap: () {
+                ref
+                    .read(userProfileControllerProvider.notifier)
+                    .updateUserProfile(
+                  userModel: currentUser.copyWith(isTwitterBlue: true),
+                  context: context,
+                  bannerFile: null,
+                  profileFile: null,
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.logout,
+                size: 30,
+              ),
+              title: const Text(
+                'Log Out',
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+              onTap: () {
+                ref.read(authControllerProvider.notifier).logout(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
